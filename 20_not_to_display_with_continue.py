8be7d86bff6e6440f5544df971ee328cef548725
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-07-13 22:51:08 +0530

    Add files via upload

---
+++ 20_not_to_display_with_continue.py
@@ -0,0 +1,37 @@
+# n=0
+# while n<10:
+#     n+=1
+#     if n==6:
+#         continue
+#     print(n)
+
+
+""" 2nd type"""
+"""
+n=0
+while n<10:
+    n+=1
+    if n<5:
+        continue
+    print(n)
+"""
+
+""" 3rd type """
+"""
+n=0
+while n<10:
+    n+=1
+    if n>5:
+        continue
+    print(n)
+"""
+
+""" 4th type  """
+"""
+n=0
+while n<10:
+    n+=1
+    if n<=5:
+        continue
+    print(n)
+"""
